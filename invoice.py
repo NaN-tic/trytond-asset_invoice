261f783447e47150ba6d75a005164567fa7ae107
Guillem Barba <guillem@nan-tic.com>
2016-04-12 13:14:12 +0200

    Define metaclass at class level

--- invoice.py
+++ invoice.py
@@ -2,14 +2,14 @@
 # copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.pyson import If, Bool, Eval
+from trytond.pyson import Eval
 
 __all__ = ['InvoiceLine', 'Sale', 'SaleLine']
-__metaclass__ = PoolMeta
 
 
 class InvoiceLine:
     __name__ = 'account.invoice.line'
+    __metaclass__ = PoolMeta
     invoice_asset = fields.Many2One('asset', 'Asset',
         states={
             'invisible': Eval('type') != 'line',
@@ -25,7 +25,7 @@ class InvoiceLine:
 
 class Sale:
     __name__ = 'sale.sale'
-
+    __metaclass__ = PoolMeta
     asset = fields.Many2One('asset', 'Asset',
         domain=[
             ('owner', '=', Eval('party')),
@@ -38,7 +38,7 @@ class Sale:
 
 class SaleLine:
     __name__ = 'sale.line'
-
+    __metaclass__ = PoolMeta
     asset_used = fields.Function(fields.Many2One('asset', 'Asset'),
         'on_change_with_asset_used')
 
