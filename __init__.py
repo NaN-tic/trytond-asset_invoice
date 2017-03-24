2fdb103bdce26be168bb23f05c914998cdff9030
?ngel ?lvarez <angel@nan-tic.com>
2017-03-24 11:23:39 +0100

    modify import wit new structure

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
-from .sale import *
+from . import invoice
+from . import sale
 
 
 def register():
     Pool.register(
-        InvoiceLine,
-        Sale,
-        SaleLine,
+        invoice.InvoiceLine,
+        sale.Sale,
+        sale.SaleLine,
         module='asset_invoice', type_='model')
